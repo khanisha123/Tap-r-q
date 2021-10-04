7d241ee0fe7de774c64e50a2eff8e8f53fd735c4
khanisha123 <khanisha@code.edu.az>
2021-10-04 23:17:26 +0400

    Add project files.

---
+++ Ccc/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Ccc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //1.Sinifdə işlədiyimiz taskı bir daha təkrar yazmaq.
+            //int input = 37;
+            //int resualt = input;
+
+            //if (input%7==0)
+            //{
+            //    Console.WriteLine(input+" 7 ye bolunur");
+            //}
+            //else
+            //{
+            //    int remainder = input % 7;
+            //    if (remainder>3)
+            //    {
+            //        while (resualt%7!=0)
+            //        {
+            //            resualt++;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        while (resualt%7!=0)
+            //        {
+            //            resualt--;
+            //        }
+            //    }
+            //    Console.WriteLine(resualt);
+            #endregion
+
+
+
+
+
+
+            //Cəm(10);
+        }    
+        //2.  Bir int parametr qəbul edən method yazırsız.O ədədə qədər bütün cüt ədədlərin cəmini ekrana yazdırın
+        //public static int Cəm(int num1)
+        //{
+        //    return num1;
+        //    int sum = 0;
+
+        //    for (int i = 0; i <=num1; i++)
+        //    {
+        //        if (i%2==0)
+        //        {
+        //            sum++;
+        //        }
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+
+
+
+    }
+    
+}
